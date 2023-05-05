9636eea79d5a4db378ca59010e9ecf4973de7d73
Hasala Abhilasha <hasalaonline@gmail.com>
2023-05-05 13:43:18 +0530

    mobile dropdown menu added

--- script.js
+++ script.js
@@ -3,5 +3,12 @@ function scrollToSection(sectionID, btn) {
     section.style.display="block";
     section.scrollIntoView({behavior: "smooth"});
     document.getElementById(btn).style.display="none";
-
 }
+
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
